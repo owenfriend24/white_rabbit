180ec046d2247660ef68f53f1b492c92ca773939
owenfriend24 <owenwfriend@gmail.com>
2026-02-02 16:48:28 -0600

    similarity searchlight testing

--- bin/searchlight_function_byrun.py
+++ bin/searchlight_function_byrun.py
@@ -73,13 +73,13 @@ class searchlight_function_byrun(Measure):
         same_context = numpy.array(same_context)
         diff_context = numpy.array(diff_context)
 
-        print(f'same context: shape = {same_context.shape}, min = {same_context.min()}, max = {same_context.max()}')
+        #print(f'same context: shape = {same_context.shape}, min = {same_context.min()}, max = {same_context.max()}')
 
-        print(f'diff context: shape = {diff_context.shape}, min = {diff_context.min()}, max = {diff_context.max()}')
+        #print(f'diff context: shape = {diff_context.shape}, min = {diff_context.min()}, max = {diff_context.max()}')
 
 
         same_over_diff = numpy.nanmean(same_context) - numpy.nanmean(diff_context)
-        print(f'same over diff: {same_over_diff}')
+        #print(f'same over diff: {same_over_diff}')
 
         # contrast conditions
         return numpy.array([same_over_diff], dtype=numpy.float32)
