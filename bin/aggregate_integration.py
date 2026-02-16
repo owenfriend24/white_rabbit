186e0129bb79f7bb4ca458c47a2b3a5f32d7ad90
owenfriend24 <owenwfriend@gmail.com>
2026-02-16 17:03:29 -0600

    update profile

--- bin/aggregate_integration.py
+++ bin/aggregate_integration.py
@@ -32,12 +32,12 @@ def aggregate_csv_files(comparison, csv_files, master_dir, mask):
     else:
         print("No CSV files were found for aggregation.")
 
-def is_processed (sub, comparison, master_dir, mask, measure):
+def is_processed (sub, comparison, master_dir, mask):
     ps_file = Path(f"{master_dir}/sub-{sub}/PS_{mask}_{comparison}/sub-{sub}_{comparison}_{mask}_master.csv")
 
     return ps_file.exists()
 
-def main(measure, master_dir, comparison, mask, agg_file):
+def main(master_dir, comparison, mask, agg_file):
     subjects = get_age_groups.get_all_subjects()
     excludes = []
 
@@ -58,7 +58,7 @@ def main(measure, master_dir, comparison, mask, agg_file):
        # drop_run = drop_runs.get(sub)
        # drop_flag = f"--drop_run {drop_run} " if drop_run else ""
 
-        sub_processed = is_processed(sub, comparison, master_dir, mask, measure)
+        sub_processed = is_processed(sub, comparison, master_dir, mask)
 
         if not sub_processed:
             print(f"Processing prepost values for {sub}...")
@@ -83,4 +83,4 @@ if __name__ == "__main__":
                         default=False, help="write aggregate file - boolean")
 
     args = parser.parse_args()
-    main(args.measure, args.master_dir, args.comparison, args.mask, args.agg_file)
+    main(args.master_dir, args.comparison, args.mask, args.agg_file)
